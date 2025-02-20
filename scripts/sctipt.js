ae2dc385a08ac2024519b8760c1c26daa897d2ae
tarabah197 <58168147+tarabah197@users.noreply.github.com>
2025-02-20 17:17:14 +0000

    add: create functions to get value of operators & numbers

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -1,3 +1,26 @@
+const operatorContainer = document.querySelector('#operators');
+const numbersContainer = document.querySelector('#numbers');
+let getNumber = '';
+let getOperator = '';
+
+
+numbersContainer.onclick = function (event) {
+    let target = event.target;
+
+    if (target.tagName != 'DIV') return;
+
+    return getNumber = target.textContent;
+};
+
+operatorContainer.onclick = function (event) {
+    let target = event.target;
+
+    if (target.tagName != 'DIV') return;
+
+    return getOperator = target.textContent;
+};
+
+
 function add (a,b){
     return a + b;
 }

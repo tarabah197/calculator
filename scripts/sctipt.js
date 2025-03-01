25c258b0627de04b052253b6f5e395e4586f50e3
tarabah197 <58168147+tarabah197@users.noreply.github.com>
2025-03-01 16:03:00 +0000

    add: create function update display info, clear function, add numbers and operator fix: style and html structure

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -1,18 +1,52 @@
 const btnContainer = document.querySelector('.btn-container');
+const displayCurrentValue = document.querySelector('.current');
+const displayPreviousValue = document.querySelector('.previous');
 const numbers = ['0','1','2','3','4','5','6','7','8','9','.'];
 const operators = ['+','-','/','*'];
-let getNumber = '';
+let a = '';
+let b = '';
+let operator = '';
+let operation = [displayCurrentValue.textContent.trim()];
 
+function clearAll() {
+    displayCurrentValue.textContent = 0;
+    displayPreviousValue.textContent = '';
+    a = '';
+    b = '';
+    operator = '';
+    updateDisplay();
+}
 
 btnContainer.onclick = function (event) {
     let target = event.target;
 
     if (target.tagName != 'BUTTON') return;
-    getNumber = target.textContent;
-    if (numbers.includes(getNumber)) 
-    return console.log("true");
-    return console.log(getNumber);
+    
+    const key = target.textContent;
+
+    // clear display
+    if (key == 'AC') return clearAll();
+
+    // get numbers and operator
+    if (numbers.includes(key)) {
+        if(b === '' && operator === ''){        
+            a+=key;
+        }else if (a !== '' && operator !== '') {
+            b+=key;
+        }
+    } 
+    if (operators.includes(key)){
+        operator = key;
+    }
+
+    updateDisplay();
 };
+
+function updateDisplay() {
+    displayCurrentValue.textContent = `${a} ${operator} ${b}`;
+    displayPreviousValue.textContent = a !== '' && operator !== '' && b !== '' ? `${a} ${operator} ${b} =` : '';
+}
+
 
 
 function add (a,b){
